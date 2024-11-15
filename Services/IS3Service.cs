870bb14995b531db77547bef72cbe80ac50faae0
Matthew Alexis Pascua <162862318+M4TTH3WW@users.noreply.github.com>
2024-11-15 11:32:00 +0800

    Add files via upload

---
+++ Services/IS3Service.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LaundryAPI.Services
+{
+    public interface IS3Service
+    {
+        Task<string> UploadFileAsync(IFormFile file);
+        Task<byte[]> DownloadFileAsync(string fileKey);
+    }
+}
